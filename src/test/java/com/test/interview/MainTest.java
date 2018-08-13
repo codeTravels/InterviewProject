b0df2aa15f2fe9a40165d41a4bfcda9a7019c51e
Cory Bianchi <you@example.com>
2018-08-13 22:28:10 +0200

    made changes to run main from test area easier

--- src/test/java/com/test/interview/MainTest.java
+++ src/test/java/com/test/interview/MainTest.java
@@ -1,5 +1,8 @@
 package com.test.interview;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 /**
@@ -11,8 +14,14 @@ public class MainTest
 
     @Test
     @org.junit.Ignore
-    public void testMain()
+    public void testMain() throws IOException
     {
+        File file = new File("hsqldb");
+        if (file.exists())
+        {
+            System.out.println("deleting hsqldb directory");
+            FileUtils.deleteDirectory(file);
+        }
         Main.main(new String[]
         {
             "--file", "src\\test\\resources\\inputFile.txt"
